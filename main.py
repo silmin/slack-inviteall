64b70eb55e49184ce245987f73fbe13e27350c03
silmin <silvestmint@gmail.com>
2019-07-26 15:44:24 +0900

    add: get target channels

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
+import sys
 import json
 import requests
 
 import mytoken
 
 token = mytoken.token
-url = "https://slack.com/api/users.list"
-payload = {"token": token}
 
-# res: ["members"][idx]["name"] -> [@]name
-res = requests.get(url, params=payload).json()
+def isUser(idx):
+    return not(idx["deleted"] or idx["is_bot"] or idx["is_app_user"])
 
-# nameを抽出して@nameのみのlistを作成
-users = ["@" + idx["name"] for idx in res["members"]]
+def getChannels(name):
+    url = "https://slack.com/api/channels.list"
+    payload = {"token": token}
+
+    # res: ["channels"][idx]["name"] -> [#]name
+    res = requests.get(url, params=payload).json()
+
+    # 欲しいchannelを抽出してname:idのdictを作成
+    targets = {idx["name"]:idx["id"] for idx in res["channels"] if idx["name"] == name}
+
+    return targets
+
+def getUsers():
+    url = "https://slack.com/api/users.list"
+    payload = {"token": token}
+
+    # res: ["members"][idx]["name"] -> [@]name
+    res = requests.get(url, params=payload).json()
+
+    # userを抽出してname:idのdictを作成
+    users = {idx["name"]:idx["id"] for idx in res["members"] if isUser(idx)}
+
+    return users
+
+
+if __name__ == "__main__" :
+    channels = getChannels(sys.argv[1])
+    users = getUsers()
+    print(channels)
+    print(users)
+    
