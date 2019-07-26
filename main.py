ce677cf5710f6e1f78ea4b71cd943bfdbd11a60d
silmin <silvestmint@gmail.com>
2019-07-26 12:51:38 +0900

    add: extract only username

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import slack
+import json
+import requests
 
 import mytoken
 
 token = mytoken.token
-client = slack.WebClient(token=token)
+url = "https://slack.com/api/users.list"
+payload = {"token": token}
 
-# res: ["member"][idx]["name"] -> @name
-res = client.users_list()
+# res: ["members"][idx]["name"] -> [@]name
+res = requests.get(url, params=payload).json()
 
+# nameを抽出して@nameのみのlistを作成
+users = ["@" + idx["name"] for idx in res["members"]]
